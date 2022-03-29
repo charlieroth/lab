9d4462905cffa091f97e6c12cbb535a059e4bf83
Charles Roth <charlieroth4@icloud.com>
2022-03-29 13:36:24 +0200

    2022-03-29

--- go/go-http/main.go
+++ go/go-http/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
 	env "github.com/charlieroth/lab/go/go-http/env"
 	"github.com/gofiber/fiber/v2"
-  "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/jackc/pgx/v4"
 )
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func DatabaseURL() string {
+	return fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+}
 
 func main() {
 	err := env.Load(".env")
@@ -15,27 +34,42 @@ func main() {
 		panic(err)
 	}
 
+	url := DatabaseURL()
+	conn, err := pgx.Connect(context.Background(), url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	app := fiber.New()
 
-  app.Use(logger.New())
+	app.Use(logger.New())
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"msg": "pong"})
 	})
 
-	app.Post("/echo", func(c *fiber.Ctx) error {
-		type EchoRequestBody struct {
-			Msg string `json:"msg"`
-		}
+  app.Get("/people", func(c *fiber.Ctx) error {
+    rows, _ := conn.Query(context.Background(), "SELECT name as Name, age as Age FROM people;")
 
-		body := new(EchoRequestBody)
-		if err := c.BodyParser(body); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": "failed to parse request body"})
-		}
+    var people []Person
 
-		return c.Status(200).JSON(fiber.Map{"message": body.Msg})
-	})
+    for rows.Next() {
+      p := Person{}
 
-  addr := fmt.Sprintf(":%s", env.Get("PORT"))
+      err := rows.Scan(&p.Name, &p.Age)
+      if err != nil {
+        fmt.Fprintf(os.Stderr, "rows.Scan failed: %v\n", err)
+        os.Exit(1)
+      }
+
+      people = append(people, p)
+    }
+		
+    return c.JSON(people)
+  })
+
+	addr := fmt.Sprintf(":%s", env.Get("PORT"))
 	log.Fatal(app.Listen(addr))
 }
---
+++ node/node-http/packages/service-a/db/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE people(
+  name character varying(255),
+  age int
+);
+
+INSERT INTO people(name, age) VALUES ('Charlie', 26), ('Miranda', 25);
