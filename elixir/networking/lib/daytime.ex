06cef7c07d4b17fd59769631f266995ac79cc9fe
charlieroth <charlieroth4@icloud.com>
2023-03-22 18:24:46 +0100

    Add testing documentation to Daytime and Echo mods

--- elixir/networking/lib/daytime.ex
+++ elixir/networking/lib/daytime.ex
@@ -1,6 +1,16 @@
 defmodule Daytime do
   @moduledoc """
   RFC867 (Daytime) Protocol
+
+  To test this, run the following in an iex session:
+
+  ```
+    iex(1)> {:ok, pid} = ThousandIsland.start_link(handler_module: Daytime)
+    iex(2)> {:ok, conn} = Mint.HTTP.connect(:http, "localhost", 4000)
+    iex(3)> flush()
+    {:tcp, #Port<0.8>, "2023-03-22 16:48:34.631675Z"}
+    :ok
+  ```
   """
   use ThousandIsland.Handler
 
