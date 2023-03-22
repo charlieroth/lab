03c91611d2d5de10a88b5b6d183fcbe3055fa0bd
charlieroth <charlieroth4@icloud.com>
2023-03-22 18:24:26 +0100

    Add mint dep

--- elixir/networking/mix.exs
+++ elixir/networking/mix.exs
@@ -24,7 +24,9 @@ defmodule Networking.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:thousand_island, "~> 0.6.4"}
+      {:thousand_island, "~> 0.6.4"},
+      {:castore, "~> 1.0"},
+      {:mint, "~> 1.0"}
     ]
   end
 end
