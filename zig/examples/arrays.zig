ccc4a4d2782b0b54ada062035f46324697271ce2
charlieroth <charlieroth4@icloud.com>
2023-03-22 17:09:30 +0100

    deleted zig, go stuff

--- zig/examples/arrays.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    const nums = [10]i32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    const nums_len = nums.len;
-    std.debug.print("nums[{d}] = {d}\n", .{5, nums[5]});
-    std.debug.print("nums length = {d}\n", .{nums_len});
-}
