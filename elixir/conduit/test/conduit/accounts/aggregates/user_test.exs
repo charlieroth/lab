322659e1bc842cd9c1fda0775564b5b475483902
charlieroth <charlieroth4@icloud.com>
2023-03-25 16:03:31 +0100

    First User aggregate unit test

---
+++ elixir/conduit/test/conduit/accounts/aggregates/user_test.exs
@@ -0,0 +1,24 @@
+defmodule Conduit.Accounts.Aggregates.UserTest do
+  use Conduit.AggregateCase, aggregate: Conduit.Accounts.Aggregates.User
+
+  alias Conduit.Accounts.Events.UserRegistered
+
+  describe "register user" do
+    @tag :unit
+    test "should succeed when valid" do
+      user_uuid = UUID.uuid4()
+
+      assert_events(
+        build(:register_user, user_uuid: user_uuid),
+        [
+          %UserRegistered{
+            user_uuid: user_uuid,
+            username: "test",
+            email: "test@email.com",
+            hashed_password: "pass123"
+          }
+        ]
+      )
+    end
+  end
+end
