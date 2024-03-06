107fd6e7e24c2c1e3cb284b3e92066aa3ca953fa
charlieroth <charlieroth4@icloud.com>
2024-03-06 13:55:45 +0100

    Add Merriweather fonts

--- local-first/merge-md/tailwind.config.js
+++ local-first/merge-md/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Merriweather Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
-}
-
+};
