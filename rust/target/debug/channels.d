54c785224c321333acca68224d7ee048aa678c84
charlieroth <charlieroth4@gmail.com>
2025-01-06 11:16:26 +0100

    [rust]: simple channel example

---
+++ rust/target/debug/channels.d
@@ -0,0 +1,1 @@
+/Users/charlie/github.com/charlieroth/lab/rust/target/debug/channels: /Users/charlie/github.com/charlieroth/lab/rust/channels/src/main.rs
---
+++ rust/target/debug/deps/channels-6d8ef5967f594bb8.d
@@ -0,0 +1,5 @@
+/Users/charlie/github.com/charlieroth/lab/rust/target/debug/deps/channels-6d8ef5967f594bb8: channels/src/main.rs
+
+/Users/charlie/github.com/charlieroth/lab/rust/target/debug/deps/channels-6d8ef5967f594bb8.d: channels/src/main.rs
+
+channels/src/main.rs:
