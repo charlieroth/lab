cdd11dc410d52693993c0dc0bd5405ac6a43ed7d
charlieroth <charlieroth4@gmail.com>
2025-01-06 12:32:53 +0100

    [rust]: implement sender count to test the case of a dropped sender

--- rust/panama/src/lib.rs
+++ rust/panama/src/lib.rs
@@ -1,16 +1,42 @@
 use std::collections::VecDeque;
 use std::sync::{Arc, Condvar, Mutex};
 
+pub struct Inner<T> {
+    queue: VecDeque<T>,
+    senders: usize,
+}
+
+pub struct Shared<T> {
+    inner: Mutex<Inner<T>>,
+    available: Condvar,
+}
+
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 // Implement `Clone` for `Sender` manually because `#[derive(Clone)]` would de-sugar
 // to making `T` clonable which `Arc<Inner<T>>` takes care of
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        drop(inner);
+
         Sender {
-            inner: Arc::clone(&self.inner),
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let was_last = inner.senders == 0;
+        drop(inner);
+        if was_last {
+            self.shared.available.notify_one();
         }
     }
 }
@@ -18,58 +44,59 @@ impl<T> Clone for Sender<T> {
 impl<T> Sender<T> {
     pub fn send(&mut self, t: T) {
         // Obtain lock on the queue
-        let mut queue = self.inner.queue.lock().unwrap();
+        let mut inner = self.shared.inner.lock().unwrap();
         // Push the message onto the queue
-        queue.push_back(t);
+        inner.queue.push_back(t);
         // Drop the lock
-        drop(queue);
+        drop(inner);
         // Notify the single receiver (thread) that a msg is available
-        self.inner.available.notify_one();
+        self.shared.available.notify_one();
     }
 }
 
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&mut self) -> T {
+    pub fn recv(&mut self) -> Option<T> {
         // Obtain a lock on the queue
-        let mut queue = self.inner.queue.lock().unwrap();
+        let mut inner = self.shared.inner.lock().unwrap();
         // Loop until a message is available (not a spin loop)
         loop {
             // Try to obtain message from queue
-            match queue.pop_front() {
-                // If message is available, return message
-                Some(t) => return t,
+            match inner.queue.pop_front() {
+                // If message is available
+                Some(t) => return Some(t),
+                // If no message available and no senders remain
+                None if inner.senders == 0 => return None,
                 // If no message available
                 None => {
                     // Block current thread until available message
                     // Gives up the lock, before sleeping, allowing sender to proceed
-                    queue = self.inner.available.wait(queue).unwrap();
+                    inner = self.shared.available.wait(inner).unwrap();
                 }
             }
         }
     }
 }
 
-pub struct Inner<T> {
-    queue: Mutex<VecDeque<T>>,
-    available: Condvar,
-}
-
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::default(),
+        queue: VecDeque::default(),
+        senders: 1,
+    };
+    let shared = Shared {
+        inner: Mutex::new(inner),
         available: Condvar::new(),
     };
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
     (
         Sender {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
         Receiver {
-            inner: inner.clone(),
+            shared: shared.clone(),
         },
     )
 }
@@ -82,6 +109,13 @@ mod tests {
     fn ping_pong() {
         let (mut tx, mut rx) = channel();
         tx.send(42);
-        assert_eq!(rx.recv(), 42);
+        assert_eq!(rx.recv(), Some(42));
+    }
+
+    #[test]
+    fn closed_tx() {
+        let (tx, mut rx) = channel::<()>();
+        drop(tx);
+        assert_eq!(rx.recv(), None);
     }
 }
